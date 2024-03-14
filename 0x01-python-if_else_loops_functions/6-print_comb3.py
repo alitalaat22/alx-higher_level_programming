92f912b05cdcdc63c8036e3f56fc49c9a16856a7
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-14 03:52:05 +0200

    donek

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(1, 10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}, ".format(i, j), end="")
+for i in range(10):
+	for u in range(i+1, 10):
+		if u == 9 and i == 8:
+			print(89)
+		else:
+			print(f"{i}{u}" ,end=', ')
