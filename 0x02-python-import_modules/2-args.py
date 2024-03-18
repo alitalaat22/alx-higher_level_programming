6012843a41b5e2f9874a0c6f35f2738500a89cd6
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-18 04:22:20 +0200

    tad

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+	"""Print the number of and list of arguments."""
+	import sys
+	t = 0
+	if len(sys.argv) == 1:
+		print(f"{len(sys.argv) - 1} arguments.")
+	elif len(sys.argv) == 2:
+		print(f"{len(sys.argv) - 1} argument")
+	else:
+		print(f"{len(sys.argv) - 1} arguments")
+	for i in range(1, len(sys.argv)):
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+		print(f"{i}: {sys.argv[i]}")
+		for t in sys.argv:
+			print(int(t))
