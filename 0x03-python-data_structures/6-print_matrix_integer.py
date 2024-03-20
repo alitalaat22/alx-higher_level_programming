998f4a3573d3ef5904eec291b2089b86448fb868
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-20 23:38:17 +0200

    tsl

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(row), end=" ")
-           
+    for r in matrix:
+        for i, n in enumerate(r):  # استخدام enumerate للحصول على فهرس العناصر
+            print("{}".format(n), end='')
+            if i != len(r) - 1:  # التأكد من أنه ليس آخر عنصر في الصف
+                print(" ", end='')  # طباعة مسافة فقط إذا لم يكن هذا آخر عنصر في الصف
+        else:
+            print()  # طباعة سطر فارغ بعد كل صف
