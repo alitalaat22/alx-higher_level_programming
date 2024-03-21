949dd7722bbab92e238aae112087288ecb80eee7
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-22 00:47:41 +0200

    tjsl

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    a = len(my_list)
+    if a == 0:
+        return None
+    else:
+        b = my_list[0]
+        for i in my_list:
+            if i > b:
+                b = i
+        return b
+    
