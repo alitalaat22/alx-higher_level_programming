28a4cc3d3be5d3656490ec81a2fef3aa5cadc1bf
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-21 04:52:09 +0200

    tsj

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Padding tuples with 0 if they are smaller than 2 elements
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    
     # Summing corresponding elements from the tuples
     sum_1 = tuple_a[0] + tuple_b[0]
     sum_2 = tuple_a[1] + tuple_b[1]
