ac0db1ae72870f0a65cdb34ba8630ca1c9d888a2
alitalaat22 <elshaaerali16@gmail.com.com>
2024-03-21 04:49:40 +0200

    tgkls

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
+    # Padding tuples with 0 if they are smaller than 2 elements
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
     
-    # إضافة قيمة صفرية إذا كانت الترتيبات أقل من عنصرين
-    if len_a < 2:
-        tuple_a += (0,)
-    if len_b < 2:
-        tuple_b += (0,)
-    
-    # جمع العناصر المتوافقة من الترتيبات
+    # Summing corresponding elements from the tuples
     sum_1 = tuple_a[0] + tuple_b[0]
-    sum_2 = tuple_a[1] + tuple_b[1] if len_a > 1 and len_b > 1 else 0
+    sum_2 = tuple_a[1] + tuple_b[1]
 
-    # إرجاع ترتيب جديد بالمجموعات
+    # Returning a tuple with the sums
     return (sum_1, sum_2)
