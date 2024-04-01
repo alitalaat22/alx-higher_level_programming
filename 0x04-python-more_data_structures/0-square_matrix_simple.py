fafd0e0fb7527682e8feb252129a98d7afc0a18b
alitalaat22 <elshaaerali16@gmail.com.com>
2024-04-01 05:16:30 +0200

    tsl

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+	row = []
+	e = []
+	for i in matrix:
+		for i in row:
+			e.append(row ** 2)
+		return e
---
+++ 0x04-python-more_data_structures/nestedloop.py
@@ -0,0 +1,5 @@
+for i in range(3):
+	print("f", end='')
+	for r in i:
+		print("#", end='')
+# print()
