e142991f0e75c8294203846845b677ecf20b97fb
alitalaat22 <elshaaerali16@gmail.com.com>
2024-04-03 10:39:19 +0200

    tasks

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,15 @@
+# #!/usr/bin/python3
+# def search_replace(my_list, search, replace):
+#     result = map(lambda s: replace if s == search else s, my_list)
+#     return list(result)
+
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    result = map(lambda s: replace if s == search else s, my_list)
-    return list(result)
+    s = []
+    for i in my_list:
+        if i == search:
+            s.append(replace)
+            
+        else:
+            s.append(i)
+    return s
