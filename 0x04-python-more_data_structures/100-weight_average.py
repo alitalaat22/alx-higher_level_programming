6d0b98b5d9e32040452cea44187d811c9c2e1c5c
alitalaat22 <elshaaerali16@gmail.com.com>
2024-04-06 08:03:52 +0200

    اللهم صلي علي سيدنا محمد صلاة

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,9 +2,9 @@
 def weight_average(my_list=[]):
     if my_list and len(my_list):
         num = 0
-        denom = 0
+        den = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
-            denom += (tup[1])
-        return (num/denom)
+            den += (tup[1])
+        return (num/den)
     return 0
