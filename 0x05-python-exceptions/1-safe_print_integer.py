5dd43e3ac10ec33d43d039d62d549d4992d20f83
alitalaat22 <elshaaerali16@gmail.com.com>
2024-04-16 19:34:12 +0200

    tslj

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+        if value == int(value):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+            return False
