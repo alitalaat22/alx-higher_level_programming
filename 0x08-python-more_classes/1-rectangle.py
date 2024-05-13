983f4a4938867b4f765bb3a8a1d64602070dc7d3
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-13 21:52:41 +0300

    _oop_

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,10 +30,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """retrieves height attribute"""
-
         return self.__height
 
     @height.setter
