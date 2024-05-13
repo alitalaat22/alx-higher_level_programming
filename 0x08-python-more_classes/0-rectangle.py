4c75952a91d1e1177c788e6b0e3f7395e8ec6434
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-13 15:54:56 +0300

    tssl

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A class that defines a rectangle"""
-
+#by ali elshaer
+""""this class defines a rectangle:"""
 
 class Rectangle:
-    """An empty class that represents a rectangle"""
+    """this empty class defines rectangle"""
     pass
