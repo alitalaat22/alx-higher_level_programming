1b86f10c2b96eb4af17868d5e679db59ba3b0daf
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-18 06:06:43 +0300

    oop

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,9 @@
 
-#!/usr/bin/python3
-def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return ", ".join(["BestSchool" for i in range(magic_string.count)])
 
+def increment(n):
+    n += 1
+
+a = n
+
+increment(a)
+print(a)
