0f1cae7e5f0640c7dca35b531b2f63e0bedbd51e
root <root@DESKTOP-7D25GVU.localdomain>
2024-05-19 09:33:54 +0300

    oop

--- air/models/base_model.py
+++ air/models/base_model.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""BaseModel that defines all common attributes/methods for other classes"""
 import uuid
 from datetime import datetime
 class BaseModel:
+    """Class from which all other classes will inherit"""
     def __init__(self, *args, **kwrgs):
+        """Initializes instance attributes
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         print(f"date of created \n{self.created_at}")
@@ -10,10 +18,16 @@ class BaseModel:
         print(f"date of update \n{self.update_at}")
 
     def save(self):
+        """updates the public instance attribute updated_at"""
         self.update_at = datetime.now()
 
     
     def to_dict(self):
+        """
+        returns a new copy of dictionary
+        added '__class__' key
+        Updated time to isoformat
+        """
         cop = self.__dict__.copy()
         cop["__class__"] = type(self).__name__
         cop["created"] = datetime.isoformat(self.created_at)
@@ -21,4 +35,5 @@ class BaseModel:
         return cop
 
     def __str__(self):
+        """Prints Classname, instance id, and dictionary"""
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
