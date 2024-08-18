4e496aab3c5c7168859cd74d7c4e7e3b04eb89c7
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-18 12:34:57 +0300

    al hamd le alla

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,7 @@ You must use the with statement
 You don’t need to manage file permission or file doesn't exist exceptions.
 """
 
+
 def read_file(filename=""):
     """
     Open and read a UTF8 file and print to stdout.
