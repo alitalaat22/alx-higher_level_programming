8f734159daa3623e6d76f22a7071abb79d4b2529
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-18 12:33:52 +0300

    al hamd le alla

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,13 +8,11 @@ You must use the with statement
 You don’t need to manage file permission or file doesn't exist exceptions.
 """
 
-
 def read_file(filename=""):
     """
     Open and read a UTF8 file and print to stdout.
     Args:
         filename
     """
-    with open(filename, encoding="UTF8") as myfile:
-        text = myfile.read()
-        print(text, end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
