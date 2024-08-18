ddd0a763e191f43fdedec98aeba6a9d97e73059a
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-18 13:03:26 +0300

    2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,12 +4,13 @@ write a func that writes a string TXT with UTF,
 return a num of caracters written
 """
 
+
 def write_file(filename="", text=""):
-	"""
-	this func with write open or create txt file  for writting
-	arg filename is file 
-	arg text contain the string 
-	return the num of craters
-	"""
-	with open(filename, "w", encoding="utf-8") as file:
-		return file.write(text)
+    """
+    this func with write open or create txt file  for writting
+    arg filename is file
+    arg text contain the string
+    return the num of craters
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
