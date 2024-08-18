c306a366c0910892b7639a2585c3461e03c4917d
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-18 12:26:52 +0300

    glj

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-my_file_0 = __import__("my_file_0.txt")
+
+"""
+Using the UTF8 way, read a text file.
+Print to stdout.
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don’t need to manage file permission or file doesn't exist exceptions.
+"""
+
+
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        text = f.read()
+    """
+    Open and read a UTF8 file and print to stdout.
+    Args:
+        filename
+    """
+    with open(filename, encoding="UTF8") as myfile:
+        text = myfile.read()
         print(text, end="")
