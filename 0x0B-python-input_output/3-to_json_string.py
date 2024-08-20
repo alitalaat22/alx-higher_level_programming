3cdcadb23a5d815a255d8bb37f83d1b7553161f8
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-20 22:33:18 +0300

    3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-
-
-
+"""
+write function convert str to json file
+"""
 import json
+
+
 def to_json_string(my_obj):
-	r = json.dumps(my_obj)
-	return r
+    """
+    This function returns the JSON representation of an object.
+    arg: my_obj is a Python object (e.g., dict, list, str, etc.)
+    The variable json_string stores the JSON formatted string.
+    Returns json_string after converting the object to JSON format.
+    """
+    json_string = json.dumps(my_obj)
+    return json_string
