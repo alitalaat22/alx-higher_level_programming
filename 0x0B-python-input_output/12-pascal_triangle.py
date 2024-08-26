35755f88e34376c5d6518a20b6d11514ccd876f2
root <root@DESKTOP-7D25GVU.localdomain>
2024-08-26 08:49:41 +0300

    12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """this module"""
+
+
+def pascal_triangle(n):
+    """dsfsfasdf"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev = triangle[-1]
+        now = [1]
+        for cal in range(1, len(prev)):
+            now.append(prev[cal - 1] + prev[cal])
+        now.append(1)
+        triangle.append(now)
+
+    return triangle
