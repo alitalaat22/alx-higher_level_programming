15d18d5d68e80133a6cd1c7286dc730347ec81c2
alitalaat22 <elshaaerali16@gmail.com>
2024-09-04 13:39:07 +0300

    done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -54,6 +54,10 @@ class Square(Rectangle):
         Args:
             value (int): The new size of the square's sides.
         """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.height = value
         self.width = value
 
