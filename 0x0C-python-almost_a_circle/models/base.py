dc589067ecb5090add8e721ae7e6bc6a2badfa99
alitalaat22 <elshaaerali16@gmail.com>
2024-09-04 19:04:19 +0300

    done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 
 
 import json
+import os
 
 
 class Base:
@@ -26,7 +27,7 @@ class Base:
         else:
             return json.dumps(list_dictionaries)
 
-    @ classmethod
+    @classmethod
     def save_to_file(cls, list_objs):
         """thsofjklasjf
         f;ladsfjlkjaf;lk;"""
@@ -39,3 +40,32 @@ class Base:
                     s.append(obj.to_dictionary())
 
                 f.write(cls.to_json_string(s))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create an instance with all attributes already set"""
+        dummy_instance = cls(1, 1)  # Use default values
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Load list of instances from JSON file"""
+        filename = cls.__name__ + ".json"
+        if not os.path.isfile(filename):
+            return []
+
+        with open(filename, "r") as f:
+            json_string = f.read()
+
+        dictionaries = cls.from_json_string(json_string)
+        instances = [cls.create(**d) for d in dictionaries]
+
+        return instances
